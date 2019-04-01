b8a51aee472752e6552b85ed7340b8406611db21
Sandeep Aggarwal <Sandeep.Aggarwal@axsesstoday.com.au>
2019-04-01 11:46:14 +1100

    Initial check in with CRUD controllers for Products

---
+++ Data/ProductContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using RetailApi.Models;
+
+namespace RetailApi.Data
+{
+    public class ProductsContext : DbContext
+    {
+        public ProductsContext(DbContextOptions<ProductsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
